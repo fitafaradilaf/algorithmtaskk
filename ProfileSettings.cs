74603913a6d33c51a95741fec0b1a7c819d8ed39
fitafaradilaf <84325453+fitafaradilaf@users.noreply.github.com>
2023-04-01 11:53:40 +0300

    Create ProfileSettings.cs

---
+++ ProfileSettings.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Prototype.Models
+{
+    public class ProfileSettings
+    {
+        public bool IsPrivate { get; set; }
+        public bool HideEmail { get; set; }
+
+        public ProfileSettings Clone()
+        {
+            return new ProfileSettings()
+            {
+                IsPrivate = IsPrivate,
+                HideEmail = HideEmail,
+            };
+        }
+    }
+}
